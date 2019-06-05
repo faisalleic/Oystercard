bb96ebf616b72e86ec54d045304470b38e29f062
Student <student@MacBook-Pro-5.local>
2019-06-05 21:14:41 +0100

    journey history method add

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-  attr_accessor :balance, :in_use, :entry_station
+  attr_accessor :balance, :entry_station, :journeys
 
   MAXIMUM_LIMIT = 90
   DEFAULT_BALANCE = 0
@@ -9,12 +9,13 @@ class Oystercard
     @balance = 0
     @in_use = false
     @entry_station
+    @exit_station
+    @journeys = []
   end
 
   def top_up(value)
-   raise "Max Balance reached. Your balance is: #{@balance}" if @balance == MAXIMUM_LIMIT
-   @balance += value
-  # @balance += value
+    raise "Max Balance reached. Your balance is: #{@balance}" if @balance == MAXIMUM_LIMIT
+    @balance += value
   end
 
   def in_journey?
@@ -25,28 +26,35 @@ class Oystercard
     balance_check
     @entry_station = station
     @in_use = true
-
   end
 
-  def touch_out
+  def touch_out(exit_station)
     @in_use = false
     deduct(MINIMUM_FARE)
+    @exit_station = exit_station
     @entry_station = nil
   end
 
   def balance_check
     raise "You do not have enough balance. Minimum needed: £1" if @balance < MINIMUM_FARE
-end
+  end
 
   def entry_station
     @entry_station
   end
 
+  def exit_station
+    @exit_station
+  end
 
 
-private
+  private
   def deduct(fare)
     @balance -= fare
   end
 
+  def journey_history
+    journey = { :entry_station => @entry_station, :exit_station => station }
+    @journeys << journey
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,10 @@ require 'oystercard'
 
 describe Oystercard do
 
-let (:station) { double :station }
-
+  let (:station) { double :station }
+  let(:entry_station) { double :station }
+  let(:exit_station) { double :station }
+  let(:journeys){ {entry_station: entry_station, exit_station: exit_station} }
 
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
@@ -28,28 +30,47 @@ let (:station) { double :station }
   end
 
   it 'checks minimum balance, raise error if < £1' do
-    expect{subject.touch_in("barking")}.to raise_error "You do not have enough balance. Minimum needed: £1"
+    expect{subject.touch_in(station)}.to raise_error "You do not have enough balance. Minimum needed: £1"
   end
 
 
   it 'touch_in changes card in use state to be true' do
     subject.top_up(10)
-    subject.touch_in("barking")
+    subject.touch_in(station)
     expect( subject.in_journey?).to eq true
   end
 
   it 'touch_out changes card in use state to be false' do
     subject.top_up(10)
-    subject.touch_in("barking")
+    subject.touch_in(station)
     #subject.touch_out
     #expect( subject.in_journey?).to eq false
-    expect{subject.touch_out}.to change{subject.balance}.by(-Oystercard::MINIMUM_FARE)
+    expect{subject.touch_out("Barking")}.to change{subject.balance}.by(-Oystercard::MINIMUM_FARE)
 
+  end
+
+  it 'has an empty list of journeys by default' do
+    oystercard = Oystercard.new
+    expect(subject.journeys).to be_empty
   end
 
   it 'saves the entry station' do
     subject.top_up(10)
     subject.touch_in(station)
     expect(subject.entry_station).to eq station
-end
+  end
+
+  it 'stores exit station' do
+    subject.top_up(10)
+    subject.touch_in(entry_station)
+    subject.touch_out(exit_station)
+    expect(subject.exit_station).to eq exit_station
+  end
+
+  it 'stores a journey' do
+    subject.top_up(10)
+    subject.touch_in(station)
+    subject.touch_out(exit_station)
+    expect(subject.journeys).not_to be_empty
+  end
 end
