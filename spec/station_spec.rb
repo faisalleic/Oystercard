ec2948806bb46995c52132b558aaf59f12cfab63
Student <student@macbook-pro-5.home>
2019-06-06 15:28:31 +0100

    added journey files

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,13 +1,14 @@
 require 'station'
 
 describe Station do
-  subject {described_class.new("Old Street", 1)}
+let(:station) {Station.new("Old Street", 1)}
+
 
   it 'knows its name' do
-    expect(subject.name).to eq("Old Street")
+    expect(station.name).to eq("Old Street")
   end
   it 'knows its zone' do
-    expect(subject.zone).to eq(1)
+    expect(station.zone).to eq(1)
   end
 
 end
